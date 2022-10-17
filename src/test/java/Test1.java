f84208d25db19325b7dd43567c7ecacaa4e675ec
SYC <1527056506@qq.com>
2022-10-17 20:57:17 +0800

    second  commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+public class Test1 {
+
+
+    @Test
+    public void t1() {
+
+
+        System.out.println("11111");
+
+        System.out.println("hot-fix");
+
+        System.out.println("!");
+    }
+}
